580d1876be3c47510599350d80e77b970949ac99
fthgncl <gencal.fatih61@gmail.com>
2026-02-27 06:10:33 +0300

    Revert "Merge remote-tracking branch 'origin/codex/investigate-plant-disease-causes'"
    This reverts commit 0114934b8ee19b0e8f24533861e1320d88ed3ae4, reversing
    changes made to 285386d6e57c2d50bf0b7c815c63d15435b63c48.

--- ekim_pumpkin.py
+++ ekim_pumpkin.py
@@ -12,38 +12,28 @@ def pumpkin_uret(pumpkin):
 	
 def pumpkin_ek():
 	olmamis_kabaklar = []
-	ilk_kabak_id = [None]
-	her_yerde_uygula(big_pumpkin_kontrol, True , [olmamis_kabaklar, ilk_kabak_id])
-	olmamis_kabaklari_gez(olmamis_kabaklar, ilk_kabak_id)
+	her_yerde_uygula(big_pumpkin_kontrol, True , [olmamis_kabaklar])
+	olmamis_kabaklari_gez(olmamis_kabaklar)
 	harvest()
 	
-
-def big_pumpkin_kontrol(olmamis_kabaklar, ilk_kabak_id):
+	
+def big_pumpkin_kontrol(olmamis_kabaklar):
 	if not can_harvest() or get_entity_type() != Entities.Pumpkin:
 		olmamis_kabaklar.append((get_pos_x(),get_pos_y()))
 		plant_smart(Entities.Pumpkin)
-		if ilk_kabak_id[0] == None and get_entity_type() == Entities.Pumpkin:
-			ilk_kabak_id[0] = measure()
 		
-def olmamis_kabaklari_gez(olmamis_kabaklar, ilk_kabak_id):
+def olmamis_kabaklari_gez(olmamis_kabaklar):
 	while True:
 		kalanlar = []
 		olmamis_var = False
 		
-		for x, y in reversed(olmamis_kabaklar):
+		for x, y in olmamis_kabaklar:
 			git(x, y)
-
-			if ilk_kabak_id[0] != None and get_entity_type() == Entities.Pumpkin and measure() == ilk_kabak_id[0]:
-				break
-
 			if not can_harvest() or get_entity_type() != Entities.Pumpkin:
 				plant_smart(Entities.Pumpkin)
-				if ilk_kabak_id[0] == None and get_entity_type() == Entities.Pumpkin:
-					ilk_kabak_id[0] = measure()
 				olmamis_var = True
 				kalanlar.append((x, y))
 
-		kalanlar.reverse()
 		olmamis_kabaklar = kalanlar
 		
 		if not olmamis_var:
