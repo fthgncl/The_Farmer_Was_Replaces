866a03d92a006c32aafa67b4a3a282bc270dd569
fthgncl <85290319+fthgncl@users.noreply.github.com>
2026-02-27 04:23:27 +0300

    Optimize pumpkin scan with initial measure id

--- ekim_pumpkin.py
+++ ekim_pumpkin.py
@@ -12,28 +12,38 @@ def pumpkin_uret(pumpkin):
 	
 def pumpkin_ek():
 	olmamis_kabaklar = []
-	her_yerde_uygula(big_pumpkin_kontrol, True , [olmamis_kabaklar])
-	olmamis_kabaklari_gez(olmamis_kabaklar)
+	ilk_kabak_id = [None]
+	her_yerde_uygula(big_pumpkin_kontrol, True , [olmamis_kabaklar, ilk_kabak_id])
+	olmamis_kabaklari_gez(olmamis_kabaklar, ilk_kabak_id)
 	harvest()
 	
-	
-def big_pumpkin_kontrol(olmamis_kabaklar):
+
+def big_pumpkin_kontrol(olmamis_kabaklar, ilk_kabak_id):
 	if not can_harvest() or get_entity_type() != Entities.Pumpkin:
 		olmamis_kabaklar.append((get_pos_x(),get_pos_y()))
 		plant_smart(Entities.Pumpkin)
+		if ilk_kabak_id[0] == None and get_entity_type() == Entities.Pumpkin:
+			ilk_kabak_id[0] = measure()
 		
-def olmamis_kabaklari_gez(olmamis_kabaklar):
+def olmamis_kabaklari_gez(olmamis_kabaklar, ilk_kabak_id):
 	while True:
 		kalanlar = []
 		olmamis_var = False
 		
-		for x, y in olmamis_kabaklar:
+		for x, y in reversed(olmamis_kabaklar):
 			git(x, y)
+
+			if ilk_kabak_id[0] != None and get_entity_type() == Entities.Pumpkin and measure() == ilk_kabak_id[0]:
+				break
+
 			if not can_harvest() or get_entity_type() != Entities.Pumpkin:
 				plant_smart(Entities.Pumpkin)
+				if ilk_kabak_id[0] == None and get_entity_type() == Entities.Pumpkin:
+					ilk_kabak_id[0] = measure()
 				olmamis_var = True
 				kalanlar.append((x, y))
 
+		kalanlar.reverse()
 		olmamis_kabaklar = kalanlar
 		
 		if not olmamis_var:
